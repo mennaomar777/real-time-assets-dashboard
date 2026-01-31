32fc89dc8f535bb19c15b60d2e2b3e833cffd45d
mennaomar777 <mennaomar764@gmail.com>
2026-01-31 20:02:26 +0200

    create project folders, add AssetType interface and mock assets data

---
+++ app/_lib/data.ts
@@ -0,0 +1,28 @@
+import { AssetType } from "../_interfaces/asset";
+
+export const data: AssetType[] = [
+  { id: 1, name: "Apple", price: 150, change: 2, category: "Tech" },
+  { id: 2, name: "Tesla", price: 720, change: -1.5, category: "Automotive" },
+  { id: 3, name: "Amazon", price: 120, change: 0.5, category: "E-commerce" },
+  { id: 4, name: "Microsoft", price: 280, change: 1.2, category: "Tech" },
+  {
+    id: 5,
+    name: "Netflix",
+    price: 500,
+    change: -0.8,
+    category: "Entertainment",
+  },
+  { id: 6, name: "Google", price: 130, change: 0.9, category: "Tech" },
+  { id: 7, name: "BMW", price: 95, change: -0.5, category: "Automotive" },
+  { id: 8, name: "Nike", price: 80, change: 1.5, category: "Fashion" },
+  { id: 9, name: "Facebook", price: 270, change: -1.1, category: "Tech" },
+  {
+    id: 10,
+    name: "Amazon Prime",
+    price: 115,
+    change: 0.3,
+    category: "Entertainment",
+  },
+  { id: 11, name: "Toyota", price: 65, change: 0.7, category: "Automotive" },
+  { id: 12, name: "Zara", price: 45, change: 1.2, category: "Fashion" },
+];
